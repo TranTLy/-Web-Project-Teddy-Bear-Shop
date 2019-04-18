7b77efc2ec4db237e2496ae11a7081bfaee877ea
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2019-04-18 20:09:57 +0700

    Thêm expresslayouts

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var expressLayouts = require('express-ejs-layouts');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminsRouter = require('./routes/admins');
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(expressLayouts);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
