d254ec53b367ed59bca33d1e944bf9c364b1689e
Tran Phu Nguyen <tranphunguyen.fit@gmail.com>
2019-04-20 14:44:02 +0700

    Tạo trang quản lý người dùng

--- routes/admins.js
+++ routes/admins.js
@@ -7,4 +7,7 @@ router.get('/', function(req, res, next) {
 router.get('/dashboard', function(req, res, next) {
   res.render('admin/pages/dashboard/index', { title: 'Dashboard1' });
 });
+router.get('/users', function(req, res, next) {
+  res.render('admin/pages/users/index', { title: 'User' });
+});
 module.exports = router;
