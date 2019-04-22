f6b0d629ab2787e350a26665ef37df78e5737990
Trần Phú Nguyện 1 <tranphunguyen.fit@gmail.com>
2019-04-22 17:46:33 +0700

    Reset commit trước khi merge (nhầm)

--- routes/index.js
+++ routes/index.js
@@ -1,81 +1,8 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
-// /* GET home page. */
-// router.get("/", function(req, res, next) {
-//   res.render("index", { title: "Express" });
-// });
-
 /* GET home page. */
-router.get("/", function(req, res, next) {
-  res.render("customer-views/index", { title: "Home" });
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Page' });
 });
-/* GET home page. */
-router.get("/index", function(req, res, next) {
-  res.render("customer-views/index", { title: "Home" });
-});
-
-router.get("/about", function(req, res, next) {
-  res.render("customer-views/about", { title: "About" });
-});
-
-router.get("/checkout", function(req, res, next) {
-  res.render("customer-views/checkout", { title: "Shoping cart" });
-});
-router.post("/checkout", function(req, res, next) {
-  res.render("customer-views/checkout", { title: "Shoping cart" });
-});
-
-router.get("/contact", function(req, res, next) {
-  res.render("customer-views/contact", { title: "Contact" });
-});
-
-// router.get("/icon", function(req, res, next) {
-//   res.render("customer-views/icon", { title: "Icon" });
-// });
-
-router.get("/payment", function(req, res, next) {
-  res.render("customer-views/payment", { title: "Payment" });
-});
-
-router.post("/payment", function(req, res, next) {
-  res.render("customer-views/payment", { title: "Payment" });
-});
-
-router.get("/product", function(req, res, next) {
-  res.render("customer-views/product", { title: "Product" });
-});
-
-router.get("/service", function(req, res, next) {
-  res.render("customer-views/service", { title: "Service" });
-});
-
-router.get("/shop", function(req, res, next) {
-  res.render("customer-views/shop", { title: "Shop" });
-});
-//TODO: add id
-router.get("/single", function(req, res, next) {
-  //todo: get name product form id
-  const name = "Gấu teddy";
-  res.render("customer-views/single", { title: "Single", nameProduct: name });
-});
-//todo
-router.post("/single", function(req, res, next) {
-  //add a comment
-  res.render("customer-views/single", { title: "Add a comment" });
-});
-
-router.get("/404", function(req, res, next) {
-  res.render("customer-views/single", { title: "Single" });
-});
-router.get("/*", function(req, res, next) {
-  res.render("404-not-found", { title: "Single" });
-});
-router.post("/*", function(req, res, next) {
-  res.render("404-not-found", { title: "Single" });
-});
-
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Page' });
-// });
 module.exports = router;
