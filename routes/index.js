88797be4ad986230e1564ec3259349169eef9986
MyLinhIT <1612334@student.hcmus.edu.vn>
2019-05-27 18:59:41 +0700

    Tạo trang đăng nhập, đăng ký

--- routes/index.js
+++ routes/index.js
@@ -9,39 +9,46 @@ var types_controller = require("../controllers/typesController");
 var producers_controller = require("../controllers/producersController");
 var origins_controller = require("../controllers/originsController");
 
-router.get('/',dashboard_controller.index);
+router.get("/", dashboard_controller.index);
 
-router.get('/dashboard',dashboard_controller.index);
-router.post('/dashboard',dashboard_controller.statistical);
+router.get("/dashboard", dashboard_controller.index);
+router.post("/dashboard", dashboard_controller.statistical);
 
-router.get('/users',users_controller.index);
-router.get('/users/getUser',users_controller.getUsers);
+router.get("/users", users_controller.index);
+router.get("/users/getUser", users_controller.getUsers);
 
-router.get('/products',products_controller.index);
-router.get('/products/get',products_controller.get);
-router.post('/products',products_controller.create);
-router.delete('/products/:_id',products_controller.delete);
-router.put('/products/:_id',products_controller.update);
+router.get("/products", products_controller.index);
+router.get("/products/get", products_controller.get);
+router.post("/products", products_controller.create);
+router.delete("/products/:_id", products_controller.delete);
+router.put("/products/:_id", products_controller.update);
 
-router.get('/types',types_controller.index);
-router.get('/types/get',types_controller.get);
-router.post('/types',types_controller.create);
-router.delete('/types/:_id',types_controller.delete);
-router.put('/types/:_id',types_controller.update);
+router.get("/types", types_controller.index);
+router.get("/types/get", types_controller.get);
+router.post("/types", types_controller.create);
+router.delete("/types/:_id", types_controller.delete);
+router.put("/types/:_id", types_controller.update);
 
-router.get('/origins',origins_controller.index);
-router.get('/origins/get',origins_controller.get);
-router.post('/origins',origins_controller.create);
-router.delete('/origins/:_id',origins_controller.delete);
-router.put('/origins/:_id',origins_controller.update);
+router.get("/origins", origins_controller.index);
+router.get("/origins/get", origins_controller.get);
+router.post("/origins", origins_controller.create);
+router.delete("/origins/:_id", origins_controller.delete);
+router.put("/origins/:_id", origins_controller.update);
 
-router.get('/producers',producers_controller.index);
-router.get('/producers/get',producers_controller.get);
-router.post('/producers',producers_controller.create);
-router.delete('/producers/:_id',producers_controller.delete);
-router.put('/producers/:_id',producers_controller.update);
+router.get("/producers", producers_controller.index);
+router.get("/producers/get", producers_controller.get);
+router.post("/producers", producers_controller.create);
+router.delete("/producers/:_id", producers_controller.delete);
+router.put("/producers/:_id", producers_controller.update);
 
-router.get('/bills',bills_controller.index);
-router.post('/bills',bills_controller.search_sort);
+router.get("/bills", bills_controller.index);
+router.post("/bills", bills_controller.search_sort);
 
+router.get("/register", function(req, res) {
+  res.render("pages/register/index", { title: "Đăng ký" });
+});
+
+router.get("/login", function(req, res) {
+  res.render("pages/login/index", { title: "Đăng nhập" });
+});
 module.exports = router;
